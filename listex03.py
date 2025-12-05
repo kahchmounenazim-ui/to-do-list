4468165c40b8dade0571772107f7c5f6ca9d3712
NAZIM <kahchmounenazim@gmail.com>
2025-12-05 03:27:48 +0100

    Add files via upload

---
+++ listex03.py
@@ -0,0 +1,6 @@
+#list of tasks
+tasks = ["buy groceries " ,"call dentist" , "finish sport"]
+tasks.append("read quran")
+tasks.remove("finish sport")
+#display all remaining tasks
+print("remaining tasks")
